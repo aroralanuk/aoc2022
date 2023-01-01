6f7fb94ae9be0a586dc851096405fcdd526fb4af
Kunal Arora <kuarora@ucsd.edu>
2023-01-01 11:00:30 -0800

    day 9 done

--- src/day_09_rope_bridge.rs
+++ src/day_09_rope_bridge.rs
@@ -1,7 +1,8 @@
 use crate::input_reader;
 use std::collections::HashSet;
+use itertools::Itertools;
 
-#[derive(Eq, Hash, PartialEq, Clone, Copy)]
+#[derive(Eq, Hash, PartialEq, Clone, Copy, Debug)]
 struct GridPos {
     x: isize,
     y: isize,
@@ -44,8 +45,49 @@ fn part1(input: &String) {
     println!("PART 1 - Visited: {}", visited.len());
 }
 
+fn part2(input: &String) {
+    let mut rope = vec![GridPos {x: 0, y: 0}; 10];
+    let mut visited: HashSet<GridPos> = HashSet::new();
+    visited.insert(rope[9]);
+
+    input.lines().for_each(|line| {
+        let (dir, steps) = line.split_once(' ').unwrap();
+        let steps = steps.parse::<usize>().unwrap();
+
+        for _ in 0..steps {
+            match dir {
+                "U" => rope[0].y += 1,
+                "D" => rope[0].y -= 1,
+                "L" => rope[0].x -= 1,
+                "R" => rope[0].x += 1,
+                _ => panic!("Unknown direction: {}", dir),
+            }
+
+            for i in 0..rope.len() - 1 {
+                let distance = GridPos {
+                    x: rope[i].x - rope[i + 1].x,
+                    y: rope[i].y - rope[i + 1].y,
+                };
+
+                let touching = distance.x.abs() <= 1 && distance.y.abs() <= 1;
+
+                if !touching {
+                    rope[i + 1].x += distance.x.signum();
+                    rope[i + 1].y += distance.y.signum();
+
+                    if i + 1 == rope.len() - 1 {
+                        visited.insert(rope[i+1]);
+                    }
+                }
+            }
+        }
+    });
+    println!("Visited: {}", visited.len());
+}
+
 pub fn main() {
     let input = input_reader::read_file_in_cwd("src/input/day_09.txt");
 
     part1(&input);
+    part2(&input);
 }
